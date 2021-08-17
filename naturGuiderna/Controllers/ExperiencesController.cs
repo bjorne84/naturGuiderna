d8d04ad26203152462ad9e8765080bcfddcad286
Björn Edin <edinbjorn@gmail.com>
2021-08-17 21:17:49 +0200

    created controller for experince

---
+++ naturGuiderna/Controllers/ExperiencesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using naturGuiderna.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace naturGuiderna.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        private readonly NatureDbContext _context;
+
+
+        public ExperiencesController(NatureDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Experiences.ToList();
+            return View();
+        }
+    }
+}
