224962f60817da664d303912fc95b19462dbef0e
Björn Edin <edinbjorn@gmail.com>
2021-08-20 22:27:54 +0200

    fixed navbar css and changed to natureActivitiesController from HomeController as startpage

--- naturGuiderna/Controllers/HomeController.cs
+++ naturGuiderna/Controllers/HomeController.cs
@@ -23,11 +23,7 @@ namespace naturGuiderna.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
